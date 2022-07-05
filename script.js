206dc508cd0b7152531dee7e91aa2a2eb4426b2a
Tarek <83262890+tarek102@users.noreply.github.com>
2022-07-05 16:23:28 +0200

    create selectors in script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Selectors
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
+const addBtn = document.querySelector('.add-btn');
+const booksList = document.querySelector('.books-list');
+
+
+// Event Listeners 
+
+// Functions
