da13300204562ef0a418922f8933a612eac2a63a
Tarek <83262890+tarek102@users.noreply.github.com>
2022-07-05 16:33:00 +0200

    create Elements

--- script.js
+++ script.js
@@ -7,4 +7,30 @@ const booksList = document.querySelector('.books-list');
 
 // Event Listeners 
 
+addBtn.addEventListener('click', addBook)
+
 // Functions
+
+function addBook() {
+
+  // create book Div
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('book');
+
+  // Create title
+  const titleElement = document.createElement('h3');
+  titleElement.innerText = titleInput.value;
+  
+  // create Author
+  const authorElement = document.createElement('h3');
+  authorElement.innerText = authorInput.value;
+
+  // Create Remove Btn
+  const removeElement = document.createElement('button');
+  removeElement.innerText = 'Remove';
+
+  bookDiv.appendChild(titleElement);
+  bookDiv.appendChild(authorElement);
+  bookDiv.appendChild(removeElement);
+  booksList.appendChild(bookDiv);
+}
