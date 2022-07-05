60ed2083607c9d8698ea3d441ee8ac9b5887440d
Tarek <83262890+tarek102@users.noreply.github.com>
2022-07-05 19:15:11 +0200

    Fix Linters errors

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
 // Selectors
-const titleInput = document.querySelector("#title-input");
-const authorInput = document.querySelector("#author-input");
-const addBtn = document.querySelector(".add-btn");
-const booksList = document.querySelector(".books-list");
-
-// Event Listeners
-
-addBtn.addEventListener("click", addBook);
-booksList.addEventListener("click", removeBook);
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
+const addBtn = document.querySelector('.add-btn');
+const booksList = document.querySelector('.books-list');
 
 // Functions
 
 function displayBooks() {
-  const books = JSON.parse(localStorage.getItem("books")) || [];
+  const books = JSON.parse(localStorage.getItem('books')) || [];
 
   if (!books) return;
-  booksList.innerHTML = "";
+  booksList.innerHTML = '';
   books.forEach((book) => {
     // create book Div
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("book");
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
 
     // Create title
-    const titleElement = document.createElement("h3");
+    const titleElement = document.createElement('h3');
     titleElement.innerText = book.title;
 
     // create Author
-    const authorElement = document.createElement("h3");
+    const authorElement = document.createElement('h3');
     authorElement.innerText = book.author;
 
     // Create Remove Btn
-    const removeElement = document.createElement("button");
-    removeElement.classList.add("remove-btn");
-    removeElement.innerText = "Remove";
-    removeElement.setAttribute("data-id", book.index);
+    const removeElement = document.createElement('button');
+    removeElement.classList.add('remove-btn');
+    removeElement.innerText = 'Remove';
+    removeElement.setAttribute('data-id', book.index);
 
     bookDiv.appendChild(titleElement);
     bookDiv.appendChild(authorElement);
@@ -44,32 +39,33 @@ function displayBooks() {
 
 function addBook() {
   // Store book to local storage
-  let books;
   if (!titleInput.value || !authorInput.value) return;
-  books = JSON.parse(localStorage.getItem("books")) || [];
-  
-  let book = {};
+  const books = JSON.parse(localStorage.getItem('books')) || [];
+  const book = {};
   book.title = titleInput.value;
   book.author = authorInput.value;
   book.index = books.length;
   books.push(book);
 
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
   titleInput.value = '';
   authorInput.value = '';
-  
+
   displayBooks();
 }
 
 function removeBook(e) {
   const item = e.target;
-  if (item.classList[0] === "remove-btn") {
-    const books = JSON.parse(localStorage.getItem("books"));
-    const indexInd = parseInt(item.dataset.id);
+  if (item.classList[0] === 'remove-btn') {
+    const books = JSON.parse(localStorage.getItem('books'));
+    const indexInd = Number(item.dataset.id);
     const modified = books.filter((book) => book.index !== indexInd);
-    localStorage.setItem("books", JSON.stringify(modified));
+    localStorage.setItem('books', JSON.stringify(modified));
   }
   displayBooks();
 }
 
-window.addEventListener("load", displayBooks);
+// Event Listeners
+booksList.addEventListener('click', removeBook);
+window.addEventListener('load', displayBooks);
+addBtn.addEventListener('click', addBook);
