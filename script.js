e265d0728057371bdc145aee089a448f58e96102
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-07 17:01:46 +0300

    Add js logic to show books list section and hide the other pages

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /* eslint-disable max-classes-per-file */
 
 // Selectors
-const addBtn = document.querySelector('.add-btn');
-const booksList = document.querySelector('.books-list');
-const allBooksSection = document.querySelector('#all-books-section');
-const addBookSection = document.querySelector('#books-section');
-const contactSection = document.querySelector('#contact-section');
-const listNavLink = document.querySelector('.list-nav-link');
-const addNavLink = document.querySelector('.add-nav-link');
-const contactNavLink = document.querySelector('.contact-nav-link');
+const addBtn = document.querySelector(".add-btn");
+const booksList = document.querySelector(".books-list");
+const allBooksSection = document.querySelector("#all-books-section");
+const addBookSection = document.querySelector("#books-section");
+const contactSection = document.querySelector("#contact-section");
+const listNavLink = document.querySelector(".list-nav-link");
+const addNavLink = document.querySelector(".add-nav-link");
+const contactNavLink = document.querySelector(".contact-nav-link");
 
 class Book {
   constructor(title, author) {
@@ -23,10 +23,10 @@ class Store {
   static getBooks() {
     let books;
 
-    if (localStorage.getItem('books') === null) {
+    if (localStorage.getItem("books") === null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem('books'));
+      books = JSON.parse(localStorage.getItem("books"));
     }
     return books;
   }
@@ -34,7 +34,7 @@ class Store {
   static addBook(book) {
     const books = Store.getBooks();
     books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   }
 
   static removeBook(author) {
@@ -46,7 +46,7 @@ class Store {
       }
     });
 
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   }
 }
 
@@ -63,30 +63,30 @@ class UI {
 
   static addBookToList(book) {
     // create book Div
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
 
     // Create title
-    const titleElement = document.createElement('h3');
+    const titleElement = document.createElement("h3");
     titleElement.innerText = book.title;
 
     // create Author
-    const authorElement = document.createElement('h3');
+    const authorElement = document.createElement("h3");
     authorElement.innerText = book.author;
 
     // Create Remove Btn
-    const removeElement = document.createElement('button');
-    removeElement.classList.add('remove-btn');
-    removeElement.innerText = 'Remove';
-    removeElement.setAttribute('data-id', book.index);
+    const removeElement = document.createElement("button");
+    removeElement.classList.add("remove-btn");
+    removeElement.innerText = "Remove";
+    removeElement.setAttribute("data-id", book.index);
 
     // Create infoDiv element
-    const infoDiv = document.createElement('div');
-    infoDiv.classList.add('info');
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("info");
 
     // create h3
-    const linkElement = document.createElement('h3');
-    linkElement.innerText = 'by';
+    const linkElement = document.createElement("h3");
+    linkElement.innerText = "by";
 
     infoDiv.appendChild(titleElement);
     infoDiv.appendChild(linkElement);
@@ -98,21 +98,21 @@ class UI {
   }
 
   static clearFields() {
-    document.getElementById('title-input').value = '';
-    document.getElementById('author-input').value = '';
+    document.getElementById("title-input").value = "";
+    document.getElementById("author-input").value = "";
   }
 
   static deleteBook(el) {
-    if (el.classList.contains('remove-btn')) {
+    if (el.classList.contains("remove-btn")) {
       el.parentElement.remove();
     }
   }
 }
 
 // Adding Book
-addBtn.addEventListener('click', () => {
-  const titleInput = document.querySelector('#title-input').value;
-  const authorInput = document.querySelector('#author-input').value;
+addBtn.addEventListener("click", () => {
+  const titleInput = document.querySelector("#title-input").value;
+  const authorInput = document.querySelector("#author-input").value;
   if (titleInput && authorInput) {
     const book = new Book(titleInput, authorInput);
     UI.addBookToList(book);
@@ -121,27 +121,29 @@ addBtn.addEventListener('click', () => {
   }
 });
 // Removing Book
-booksList.addEventListener('click', (e) => {
+booksList.addEventListener("click", (e) => {
   UI.deleteBook(e.target);
   console.log(e.target.previousElementSibling.children[2]);
   Store.removeBook(e.target.previousElementSibling.children[2].textContent);
 });
 
 // Event listener
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
-listNavLink.addEventListener('click', function(e){
-  e.preventDefault()
-  alert('list books')
-} );
-addNavLink.addEventListener('click', function(e){
-  e.preventDefault()
-  addBookSection.classList.add('show-section')
-  contactSection.classList.add('hide-section')
-  allBooksSection.classList.add('hide-section')
-} );
-contactNavLink.addEventListener('click', function(e){
-  e.preventDefault()
-  contactSection.classList.add('show-flex-section')
-  addBookSection.classList.add('hide-section')
-  allBooksSection.classList.add('hide-section')
-} );
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+listNavLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  allBooksSection.classList.add("show-section");
+  addBookSection.classList.add("hide-section");
+  contactSection.classList.add("hide-section");
+});
+addNavLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  addBookSection.classList.add("show-section");
+  contactSection.classList.add("hide-section");
+  allBooksSection.classList.add("hide-section");
+});
+contactNavLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  contactSection.classList.add("show-flex-section");
+  addBookSection.classList.add("hide-section");
+  allBooksSection.classList.add("hide-section");
+});
